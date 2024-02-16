034c9a95cdd5057ca5114e73ca8bbeea475467d2
Joe Farah <joseph.farah100@gmail.com>
2024-02-16 15:53:48 +0900

    Fix crashing bug with Show Answer

--- KoreanNumbers/ContentView.swift
+++ KoreanNumbers/ContentView.swift
@@ -7,11 +7,18 @@
 // GOAL: Application that teaches how to say a number in Korean. The answer can be in either Native-Korean or Sino-Korean
 
 
+// Fix Keypad
+// Adjust colors, add animations, background
+
+
 import SwiftUI
 
 // Styles
 struct BoldCenteredText: ViewModifier {
+    @State var opacity = 0.4
+    
     func body(content: Content) -> some View {
+        
         content
             .font(.title)
             .padding()
@@ -19,25 +26,27 @@ struct BoldCenteredText: ViewModifier {
             .listRowBackground(Color.clear)
             .foregroundStyle(Color.customKoreanBlue)
             .bold()
-        
-            
+            .animation(.spring, value: opacity)
         
     }
+
+    
 }
+
 
 struct BlueButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            
+        
             .font(.title3)
             .frame(maxWidth: .infinity, alignment: .center)
             .background(Color.customKoreanBlue)
             .foregroundStyle(.white)
             .clipShape(Capsule())
-           
+        
             .scaleEffect(configuration.isPressed ? 1.2 : 1)
-            .animation(.easeOut(duration: 0.3), value: configuration.isPressed)
+            .animation(.spring(duration: 0.2), value: configuration.isPressed)
     }
 }
 
@@ -68,7 +77,7 @@ extension View {
 }
 
 extension Color {
-    static let customKoreanBlue = Color(red: 0 / 255.0, green: 71 / 255.0, blue: 159 / 255.0)
+    static let customKoreanBlue = Color(red: 0 / 255.0, green: 71 / 255.0, blue: 140 / 255.0)
     static let customKoreanYellow = Color(red: 233 / 255.0, green: 180 / 255.0, blue: 58 / 255.0)
 }
 
@@ -76,7 +85,7 @@ struct SinoKorean: View {
     
     @State private var showAnswer = false
     @State private var buttonTitle = "Show Answer"
-    var aNumber: String
+    var sinoKoreanNumber: String
     
     var answerString: String = ""
     let ones = ["", "일","이", "삼", "사", "오", "육", "칠", "팔", "구"]
@@ -85,7 +94,7 @@ struct SinoKorean: View {
     
     //    Get the length of our number0
     func getNumberLength() -> Int {
-        return aNumber.count
+        return sinoKoreanNumber.count
     }
     
     //    Get all the place values matching the length of the number
@@ -96,7 +105,7 @@ struct SinoKorean: View {
         var digitNames : [String] = []
         //      digit is of type Char
         //      The method wholeNumberValue on Char returns an optional Int
-        for digit in aNumber {
+        for digit in sinoKoreanNumber {
             
             if let digitValue = digit.wholeNumberValue {
                 digitNames.append(ones[digitValue])
@@ -113,13 +122,15 @@ struct SinoKorean: View {
     
     func convertNumber(number: String ) -> String {
         
+        print(number)
         var convertedNumber = ""
         var groupIndex = 0
         let length = getNumberLength()
         let digitNames = getDigitNames()
         
         
-        if length < 2 {
+        if length < 2  {
+            print(convertedNumber)
             convertedNumber = digitNames[0] == "" ? "영" : digitNames[0]
         } else {
             
@@ -129,9 +140,7 @@ struct SinoKorean: View {
                 if index % 4 == 0 && index > 0 {
                     
                     convertedNumber +=
-                    (digitNames[index] == "일") ?
-                    
-                    " " + placeValues[4 + groupIndex] :
+                   
                     " " + placeValues[4 + groupIndex] + digitNames[index ]
                     
                     groupIndex += 1
@@ -162,23 +171,25 @@ struct SinoKorean: View {
         
         
         
-            
-            
-            Button(buttonTitle) {
-                
-                buttonTitle = showAnswer ? "Show Answer" : "Hide Answer"
-                showAnswer.toggle()
-                
-                
-            }.buttonStyle(BlueButton())
-            
-            
         
         
-       
-            if showAnswer {
-                Text(convertNumber(number: aNumber)).boldCenteredStyle()
-                
+        Button(buttonTitle) {
+            
+            buttonTitle = showAnswer ? "Show Answer" : "Hide Answer"
+            showAnswer.toggle()
+            
+            
+        }.buttonStyle(BlueButton())
+        
+        
+        
+        
+        
+        
+        if showAnswer && !sinoKoreanNumber.isEmpty {
+            
+            Text(convertNumber(number: sinoKoreanNumber)).boldCenteredStyle()
+            
             
         }
         
@@ -189,14 +200,15 @@ struct NativeKorean: View {
     
     @State private var showAnswer = false
     @State private var buttonTitle = "Show Answer"
-    
-    var aNumber: String
+   
+    var nativeKoreanNumber: String
+      
     let ones = ["","하나", "둘", "셋", "넷", "다섯", "여섯", "일홉", "여덟", "아홉"]
     let tens = ["", "열", "스물", "서른", "마흔", "쉰", "예순", "일흔", "여든", "아흔"]
     
     //    Get the length of our number
     func getNumberLength() -> Int {
-        return aNumber.count
+        return nativeKoreanNumber.count
     }
     
     func convertNumber(number: String) -> String {
@@ -207,12 +219,12 @@ struct NativeKorean: View {
         
         if length < 2  {
             
-            if let index = Int(aNumber) {
+            if let index = Int(nativeKoreanNumber) {
                 convertedNumber = (index == 0) ? "공" : ones[index]
             }
         } else {
             
-            if let firstDigit = Int(aNumber.prefix(1)), let secondDigit = Int(aNumber.suffix(1)) {
+            if let firstDigit = Int(nativeKoreanNumber.prefix(1)), let secondDigit = Int(nativeKoreanNumber.suffix(1)) {
                 
                 convertedNumber += tens[firstDigit] + ones[secondDigit]
                 
@@ -230,52 +242,50 @@ struct NativeKorean: View {
         
         
         
-       
+        
+        
+        Button(buttonTitle) {
             
-            Button(buttonTitle) {
-                
-                buttonTitle = showAnswer ? "Show Answer" : "Hide Answer"
-                showAnswer.toggle()
-                
-                
-            }.buttonStyle(BlueButton())
-             
-             
+            buttonTitle = showAnswer ? "Show Answer" : "Hide Answer"
+            showAnswer.toggle()
             
-         
-                
-                if showAnswer {
-                    Text(convertNumber(number: aNumber)).boldCenteredStyle()
-                    
-                }
             
+        }.buttonStyle(BlueButton())
+        
+        
+        
+        
+        
+        
+        if showAnswer && !nativeKoreanNumber.isEmpty {
+            Text(convertNumber(number: nativeKoreanNumber)).boldCenteredStyle()
             
             
         }
+        
+        
+        
     }
-
-
-
-
-
-
+}
 
 struct ContentView: View {
+    
+
     
     //    Options for the variables
     //    Range between minNumber and maxNumber
     //    The number type:  Native or Sino-Korean
     
-    @State private var typeOptions = ["Native-Korean","Sino-Korean"]
+    @State private var typeOptions = ["Native-Korean", "Sino-Korean"]
     @State private var numberType = ""
     @State private var minNumber = 0
     @State private var maxNumber = 0
     
-
+    
     //    The generated number
     //    The answer i.e. the Korean spelling of the number
     
-    @State private var currentNumber = ""
+    @State private var currentNumber = "0"
     @State private var answer = ""
     @State private var isShown = false
     
@@ -286,31 +296,30 @@ struct ContentView: View {
     
     
     
+    
+    
     var body: some View {
-        VStack {
-            
+        
+        
+        NavigationStack {
             
             List {
                 Section("Type & Number Range") {
                     VStack {
-                       
-                        Text("Type")
+                        
+                        Text("Number Type").foregroundStyle(Color.customKoreanBlue)
+                            .bold()
                         Picker("Number Type", selection: $numberType) {
                             ForEach(typeOptions, id: \.self) { type in
-                                Text(type).font(.headline)
+                                
+                                Text(type)
                                 
                             }
                         }.pickerStyle(.segmented)
-                            .padding()
-                           
-                            
-                            
-                            
-                            
-                         
+                        
                         
                             .onChange(of: numberType) {
-                                // Reset minNumber and maxNumber to 0 when the numberType changes
+                              
                                 resetNumbers()
                             }
                         
@@ -327,8 +336,7 @@ struct ContentView: View {
                                     .padding()
                                     .foregroundStyle(Color.customKoreanBlue)
                                 
-                                
-                                
+
                                 
                                 TextField("Maximum", value: $maxNumber, format: .number)
                                     .keyboardType(.numberPad)
@@ -343,23 +351,23 @@ struct ContentView: View {
                                 
                                 
                                 Picker("Minimum:", selection: $minNumber) {
-                                    ForEach(0..<100) {
+                                    ForEach((0..<100), id: \.self) {
                                         Text("\($0) ")
                                     }
                                 }
                                 .pickerStyle(.menu)
                                 .bold()
                                 .padding(2)
-                              
+                                
                                 Picker("Maximum:", selection: $maxNumber) {
-                                    ForEach(0..<100) {
+                                    ForEach((0..<100), id: \.self) {
                                         Text("\($0) ")
                                     }
                                 }
                                 .pickerStyle(.menu)
                                 .bold()
                                 .padding(2)
-                               
+                                
                                 
                                 
                                 
@@ -378,6 +386,7 @@ struct ContentView: View {
                 
                 
                 
+                
                 Section("Your number") {
                     
                     
@@ -387,13 +396,13 @@ struct ContentView: View {
                 Section("\(numberType)") {
                     
                     if numberType == "Sino-Korean" {
-                        SinoKorean(aNumber: currentNumber )
+                        SinoKorean(sinoKoreanNumber: currentNumber)
                         
                     }
                     
                     if numberType == "Native-Korean"{
                         
-                        NativeKorean(aNumber: currentNumber)
+                        NativeKorean(nativeKoreanNumber: currentNumber)
                     }
                     
                     
@@ -401,12 +410,15 @@ struct ContentView: View {
                     
                 }
             }
+            .navigationTitle("Korean Numbers")
+            
+            .listStyle(.automatic)
         }
         
         
         
-       
-      
+        
+        
         .alert(errorTitle, isPresented: $showingError) { } message: {
             Text(errorMessage)
         }
@@ -414,36 +426,38 @@ struct ContentView: View {
     }
     
     
-//   --  Functions --
+    //   --  Functions --
     
-//    Reset the numbers if an error occurs
+    //    Reset the numbers if an error occurs
     func resetNumbers() {
-        currentNumber = ""
+        
+        currentNumber = "0"
         maxNumber = 0
         minNumber = 0
+     
         
     }
     
     
-//    Returns a random number String based on the user selected range
-//    Before generating the number
-//    Check if numbers are positive and if the range is coherent
-//    Change the error message and error titles and reset numbers.
-
+    //    Returns a random number String based on the user selected range
+    //    Before generating the number
+    //    Check if numbers are positive and if the range is coherent
+    //    Change the error message and error titles and reset numbers.
+    
     func generateNumber() -> String {
         
         
         guard arePositive(min: minNumber, max: maxNumber)  else {
             
             alertError(title: "Negative number detected", message: "Please enter positive numbers")
-
+            
             resetNumbers()
             
-//          Return only empty string, error message already shown in alert.
+            //          Return only empty string, error message already shown in alert.
             return ""
             
         }
-
+        
         guard coherentRange(min: minNumber, max: maxNumber) else {
             alertError(title: "Incoherent range", message: "The minimum must be smaller or equal than the  maximum")
             resetNumbers()
